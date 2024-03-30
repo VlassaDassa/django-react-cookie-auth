83069ac4fbc4cf8ef6f44988dd35fafa24e189d2
VlassaDassa <107307197+VlassaDassa@users.noreply.github.com>
2024-03-31 00:48:20 +0300

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -17,9 +17,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authSession', #
+    'authSession',
     
-    'corsheaders', #
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
